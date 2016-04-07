8615312b9d00e5906996da3156982f78789102d2
Andreas Hennings <andreas@dqxtech.net>
2016-04-07 14:37:25 +0200

    cfrreflection: Add ClosureToConfiguratorInterface

---
+++ cfrreflection/src/CfrGen/ClosureToConfigurator/ClosureToConfiguratorInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\cfrreflection\CfrGen\ClosureToConfigurator;
+
+use Drupal\cfrapi\Context\CfrContextInterface;
+
+interface ClosureToConfiguratorInterface {
+
+  /**
+   * @param \Closure $closure
+   * @param \Drupal\cfrapi\Context\CfrContextInterface $context
+   *
+   * @return \Drupal\cfrapi\Configurator\ConfiguratorInterface
+   */
+  function closureGetConfigurator(\Closure $closure, CfrContextInterface $context);
+
+}
