19404e6e56f8b9ad6ee3c20f745c947d04c48d5e
Andreas Hennings <andreas@dqxtech.net>
2016-04-07 14:38:14 +0200

    cfrreflection: Add ClosureToConfigurator_CallbackToConfigurator

---
+++ cfrreflection/src/CfrGen/ClosureToConfigurator/ClosureToConfigurator_CallbackToConfigurator.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\cfrreflection\CfrGen\ClosureToConfigurator;
+
+use Donquixote\CallbackReflection\Callback\CallbackReflection_Function;
+use Donquixote\CallbackReflection\Util\CallbackUtil;
+use Drupal\cfrapi\Context\CfrContextInterface;
+use Drupal\cfrreflection\CfrGen\CallbackToConfigurator\CallbackToConfiguratorInterface;
+
+class ClosureToConfigurator_CallbackToConfigurator implements ClosureToConfiguratorInterface {
+
+  /**
+   * @var \Drupal\cfrreflection\CfrGen\CallbackToConfigurator\CallbackToConfiguratorInterface
+   */
+  private $callbackToConfigurator;
+
+  /**
+   * @param \Drupal\cfrreflection\CfrGen\CallbackToConfigurator\CallbackToConfiguratorInterface $callbackToConfigurator
+   */
+  function __construct(CallbackToConfiguratorInterface $callbackToConfigurator) {
+    $this->callbackToConfigurator = $callbackToConfigurator;
+  }
+
+  /**
+   * @param \Closure $closure
+   * @param \Drupal\cfrapi\Context\CfrContextInterface $context
+   *
+   * @return \Drupal\cfrapi\Configurator\ConfiguratorInterface
+   */
+  function closureGetConfigurator(\Closure $closure, CfrContextInterface $context) {
+    $refl = new \ReflectionFunction($closure);
+    $callback = new CallbackReflection_Function($refl);
+    return $this->callbackToConfigurator->callbackGetConfigurator($callback);
+  }
+}
