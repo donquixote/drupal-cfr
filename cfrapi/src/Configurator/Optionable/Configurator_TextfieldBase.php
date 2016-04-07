50a157a2eeff065180d93f423c8de5c5de75667e
Andreas Hennings <andreas@dqxtech.net>
2016-04-07 14:35:39 +0200

    cfrapi: Add Configurator_TextfieldBase

---
+++ cfrapi/src/Configurator/Optionable/Configurator_TextfieldBase.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Drupal\cfrapi\Configurator\Optionable;
+
+use Drupal\cfrapi\ConfEmptyness\ConfEmptyness_Enum;
+use Drupal\cfrapi\SummaryBuilder\SummaryBuilderInterface;
+
+/** @noinspection PhpDeprecationInspection */
+abstract class Configurator_TextfieldBase implements OptionableConfiguratorInterface {
+
+  /**
+   * @param mixed $conf
+   *   Configuration from a form, config file or storage.
+   * @param string|null $label
+   *   Label for the form element, specifying the purpose where it is used.
+   *
+   * @return array
+   */
+  function confGetForm($conf, $label) {
+    if (is_string($conf)) {
+      $conf = NULL;
+    }
+    return array(
+      '#type' => 'textfield',
+      '#title' => $label,
+      '#default_value' => $conf,
+      '#required' => TRUE,
+    );
+  }
+
+  /**
+   * @param mixed $conf
+   *   Configuration from a form, config file or storage.
+   * @param \Drupal\cfrapi\SummaryBuilder\SummaryBuilderInterface $summaryBuilder
+   *   An object that controls the format of the summary.
+   *
+   * @return mixed|string|null
+   *   A string summary is always allowed. But other values may be returned if
+   *   $summaryBuilder generates them.
+   */
+  function confGetSummary($conf, SummaryBuilderInterface $summaryBuilder) {
+    if (!is_string($conf)) {
+      return NULL;
+    }
+    return check_plain($conf);
+  }
+
+  /**
+   * @param mixed $conf
+   *   Configuration from a form, config file or storage.
+   * @param string|null $label
+   *   Label for the form element, specifying the purpose where it is used.
+   *
+   * @return array
+   */
+  function confGetOptionalForm($conf, $label) {
+    $form = $this->confGetForm($conf, $label);
+    unset($form['#required']);
+    return $form;
+  }
+
+  /**
+   * @return \Drupal\cfrapi\ConfEmptyness\ConfEmptynessInterface
+   */
+  function getEmptyness() {
+    return new ConfEmptyness_Enum();
+  }
+
+  /**
+   * @return string
+   */
+  function getEmptySummary() {
+    return '- ' . t('None') . ' -';
+  }
+
+  /**
+   * @return mixed
+   */
+  function getEmptyValue() {
+    return NULL;
+  }
+}
