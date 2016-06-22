3b74657564b5fd8f527bbe26c28bafd3601f3d85
Andreas Hennings <andreas@dqxtech.net>
2016-06-22 17:20:18 +0200

    cfrapi: Configurator_FixedValue: Support ConfToPhpInterface.

--- cfrapi/src/Configurator/Unconfigurable/Configurator_FixedValue.php
+++ cfrapi/src/Configurator/Unconfigurable/Configurator_FixedValue.php
@@ -2,7 +2,10 @@
 
 namespace Drupal\cfrapi\Configurator\Unconfigurable;
 
-class Configurator_FixedValue extends Configurator_OptionlessBase {
+use Drupal\cfrapi\ConfToPhp\ConfToPhpInterface;
+use Drupal\cfrapi\Exception\PhpGenerationNotSupportedException;
+
+class Configurator_FixedValue extends Configurator_OptionlessBase implements ConfToPhpInterface {
 
   /**
    * @var mixed
@@ -10,10 +13,17 @@ class Configurator_FixedValue extends Configurator_OptionlessBase {
   private $fixedValue;
 
   /**
-   * @param mixed $fixedValue
+   * @var string|false|null
    */
-  public function __construct($fixedValue) {
+  private $php;
+
+  /**
+   * @param mixed $fixedValue
+   * @param string|false|null $php
+   */
+  public function __construct($fixedValue, $php = NULL) {
     $this->fixedValue = $fixedValue;
+    $this->php = $php;
   }
 
   /**
@@ -25,5 +35,30 @@ class Configurator_FixedValue extends Configurator_OptionlessBase {
    */
   public function confGetValue($conf) {
     return $this->fixedValue;
+  }
+
+  /**
+   * @param mixed $conf
+   *   Configuration from a form, config file or storage.
+   *
+   * @return string
+   *   PHP statement to generate the value.
+   *
+   * @throws \Drupal\cfrapi\Exception\PhpGenerationNotSupportedException
+   * @throws \Drupal\cfrapi\Exception\InvalidConfigurationException
+   * @throws \Drupal\cfrapi\Exception\BrokenConfiguratorException
+   */
+  public function confGetPhp($conf) {
+    if (FALSE === $this->php) {
+      $type = gettype($this->fixedValue);
+      throw new PhpGenerationNotSupportedException("This fixed value of type '$type' does not support code generation.");
+    }
+    elseif (NULL === $this->php) {
+      // @todo Check if var_export() is applicable.
+      return var_export($this->fixedValue, TRUE);
+    }
+    else {
+      return $this->php;
+    }
   }
 }
