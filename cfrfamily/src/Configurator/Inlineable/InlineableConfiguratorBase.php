c5fc27e977614f3cd8d1374147cf774b85694a47
Andreas Hennings <andreas@dqxtech.net>
2016-06-22 17:29:19 +0200

    cfrfamily: InlineableConfiguratorBase: Support ConfToPhpInterface.

--- cfrfamily/src/Configurator/Inlineable/InlineableConfiguratorBase.php
+++ cfrfamily/src/Configurator/Inlineable/InlineableConfiguratorBase.php
@@ -3,9 +3,12 @@
 namespace Drupal\cfrfamily\Configurator\Inlineable;
 
 use Drupal\cfrapi\BrokenValue\BrokenValue;
+use Drupal\cfrapi\ConfToPhp\ConfToPhpInterface;
+use Drupal\cfrapi\Exception\InvalidConfigurationException;
 use Drupal\cfrapi\Util\ConfUtil;
+use Drupal\cfrfamily\IdConfToPhp\IdConfToPhpUtil;
 
-abstract class InlineableConfiguratorBase implements InlineableConfiguratorInterface {
+abstract class InlineableConfiguratorBase implements InlineableConfiguratorInterface, ConfToPhpInterface {
 
   /**
    * @var string
@@ -30,6 +33,25 @@ abstract class InlineableConfiguratorBase implements InlineableConfiguratorInter
       return new BrokenValue($this, get_defined_vars(), 'No id specified.');
     }
     return $this->idConfGetValue($id, $conf);
+  }
+
+  /**
+   * @param mixed $conf
+   *   Configuration from a form, config file or storage.
+   *
+   * @return string
+   *   PHP statement to generate the value.
+   *
+   * @throws \Drupal\cfrapi\Exception\PhpGenerationNotSupportedException
+   * @throws \Drupal\cfrapi\Exception\InvalidConfigurationException
+   * @throws \Drupal\cfrapi\Exception\BrokenConfiguratorException
+   */
+  final function confGetPhp($conf) {
+    list($id, $conf) = $this->confGetIdOptions($conf);
+    if (NULL === $id) {
+      throw new InvalidConfigurationException("Required id missing.");
+    }
+    return IdConfToPhpUtil::objIdConfGetPhp($this, $id, $conf);
   }
 
   /**
