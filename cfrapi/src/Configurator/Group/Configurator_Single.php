1f3a510612c471f6b0bcc2f3001112d012826b4c
Andreas Hennings <andreas@dqxtech.net>
2016-06-22 17:16:02 +0200

    cfrapi: Configurator_Single: Support GroupConfToPhpStatementsInterface and ConfToPhpInterface.

--- cfrapi/src/Configurator/Group/Configurator_Single.php
+++ cfrapi/src/Configurator/Group/Configurator_Single.php
@@ -5,9 +5,12 @@ namespace Drupal\cfrapi\Configurator\Group;
 use Drupal\cfrapi\BrokenValue\BrokenValue;
 use Drupal\cfrapi\BrokenValue\BrokenValueInterface;
 use Drupal\cfrapi\Configurator\ConfiguratorInterface;
+use Drupal\cfrapi\ConfToPhp\ConfToPhpInterface;
+use Drupal\cfrapi\ConfToPhp\ConfToPhpUtil;
+use Drupal\cfrapi\GroupConfToPhpStatements\GroupConfToPhpStatementsInterface;
 use Drupal\cfrapi\SummaryBuilder\SummaryBuilderInterface;
 
-class Configurator_Single implements GroupConfiguratorInterface {
+class Configurator_Single implements GroupConfiguratorInterface, GroupConfToPhpStatementsInterface, ConfToPhpInterface {
 
   /**
    * @var \Drupal\cfrapi\Configurator\ConfiguratorInterface
@@ -58,4 +61,33 @@ class Configurator_Single implements GroupConfiguratorInterface {
     return $this->configurator->confGetForm($conf, $label);
   }
 
+  /**
+   * @param mixed $conf
+   *
+   * @return string[]
+   *   PHP statements to generate the values.
+   *
+   * @throws \Drupal\cfrapi\Exception\PhpGenerationNotSupportedException
+   * @throws \Drupal\cfrapi\Exception\InvalidConfigurationException
+   */
+  public function confGetPhpStatements($conf) {
+    $php = ConfToPhpUtil::objConfGetPhp($this->configurator, $conf);
+    return array($php);
+  }
+
+  /**
+   * @param mixed $conf
+   *   Configuration from a form, config file or storage.
+   *
+   * @return string
+   *   PHP statement to generate the value.
+   *
+   * @throws \Drupal\cfrapi\Exception\PhpGenerationNotSupportedException
+   * @throws \Drupal\cfrapi\Exception\InvalidConfigurationException
+   * @throws \Drupal\cfrapi\Exception\BrokenConfiguratorException
+   */
+  public function confGetPhp($conf) {
+    $php = ConfToPhpUtil::objConfGetPhp($this->configurator, $conf);
+    return 'array(' . $php . ')';
+  }
 }
