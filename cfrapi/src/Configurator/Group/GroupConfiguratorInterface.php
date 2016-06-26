9f504662d87fb8a1b907ecfc3725bcc4713b0dd8
Andreas Hennings <andreas@dqxtech.net>
2016-06-27 01:06:25 +0200

    GroupConfiguratorInterface: Add doc comment.

--- cfrapi/src/Configurator/Group/GroupConfiguratorInterface.php
+++ cfrapi/src/Configurator/Group/GroupConfiguratorInterface.php
@@ -3,6 +3,11 @@ namespace Drupal\cfrapi\Configurator\Group;
 
 use Drupal\cfrapi\Configurator\ConfiguratorInterface;
 
+/**
+ * Configurator where the value is an array, with predefined type per key.
+ *
+ * This is useful e.g. for a function signature.
+ */
 interface GroupConfiguratorInterface extends ConfiguratorInterface {
 
 }
