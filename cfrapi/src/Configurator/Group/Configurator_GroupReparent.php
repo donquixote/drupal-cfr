0091bcbb25378503ae003e1f75491d238347b8b1
Andreas Hennings <andreas@dqxtech.net>
2016-06-27 00:54:38 +0200

    Configurator_GroupReparent: No more traits.

--- cfrapi/src/Configurator/Group/Configurator_GroupReparent.php
+++ cfrapi/src/Configurator/Group/Configurator_GroupReparent.php
@@ -2,15 +2,99 @@
 
 namespace Drupal\cfrapi\Configurator\Group;
 
-class Configurator_GroupReparent implements GroupConfiguratorInterface {
+use Drupal\cfrapi\ElementProcessor\ElementProcessor_ReparentChildren;
+use Drupal\cfrapi\SummaryBuilder\SummaryBuilderInterface;
+use Drupal\cfrapi\Util\ConfUtil;
 
-  use ReparentConfiguratorTrait, GroupConfiguratorTrait {
-    ReparentConfiguratorTrait::confGetForm insteadof GroupConfiguratorTrait;
-    ReparentConfiguratorTrait::confGetSummary insteadof GroupConfiguratorTrait;
-    ReparentConfiguratorTrait::confGetValue insteadof GroupConfiguratorTrait;
-    GroupConfiguratorTrait::confGetForm as protected parentConfGetForm;
-    GroupConfiguratorTrait::confGetSummary as protected parentConfGetSummary;
-    GroupConfiguratorTrait::confGetValue as protected parentConfGetValue;
+class Configurator_GroupReparent extends Configurator_Group {
+
+  /**
+   * @var string[][]
+   *   Format: $[$key][] = $parentKey
+   */
+  private $keysReparent = array();
+
+  /**
+   * @param string $key
+   * @param string[] $parents
+   *   Parents, indicating the new place for this key.
+   *
+   * @return $this
+   */
+  function keySetParents($key, array $parents) {
+    $this->keysReparent[$key] = $parents;
+    return $this;
+  }
+
+  /**
+   * @param mixed $conf
+   * @param string $label
+   *
+   * @return array
+   *   A form element(s) array.
+   */
+  function confGetForm($conf, $label) {
+    $conf = $this->extractConf($conf);
+    $form = parent::confGetForm($conf, $label);
+    $form['#process'][] = new ElementProcessor_ReparentChildren($this->keysReparent);
+    return $form;
+  }
+
+  /**
+   * @param mixed $conf
+   *   Configuration from a form, config file or storage.
+   * @param \Drupal\cfrapi\SummaryBuilder\SummaryBuilderInterface $summaryBuilder
+   *
+   * @return null|string
+   */
+  function confGetSummary($conf, SummaryBuilderInterface $summaryBuilder) {
+    $conf = $this->extractConf($conf);
+    return parent::confGetSummary($conf, $summaryBuilder);
+  }
+
+  /**
+   * Builds the value based on the given configuration.
+   *
+   * @param mixed[]|mixed $conf
+   *
+   * @return mixed[]|\Drupal\cfrapi\BrokenValue\BrokenValueInterface
+   */
+  function confGetValue($conf) {
+    $conf = $this->extractConf($conf);
+    $result = parent::confGetValue($conf);
+    if (!is_array($result)) {
+      return $result;
+    }
+    foreach (array_reverse($this->keysReparent) as $key => $parents) {
+      if (isset($result[$key])) {
+        $value = $result[$key];
+        unset($result[$key]);
+        if (is_array($value)) {
+          ConfUtil::confMergeNestedValue($result, $parents, $value);
+        }
+        else {
+          ConfUtil::confSetNestedValue($result, $parents, $value);
+        }
+      }
+    }
+    return $result;
+  }
+
+  /**
+   * @param mixed $conf
+   *
+   * @return mixed[]
+   */
+  private function extractConf($conf) {
+    if (!is_array($conf)) {
+      return array();
+    }
+    $confExtracted = array();
+    foreach ($this->keysReparent as $key => $parents) {
+      $confExtracted[$key] = ConfUtil::confExtractNestedValue($conf, $parents);
+      ConfUtil::confUnsetNestedValue($conf, $parents);
+    }
+    return $confExtracted + $conf;
   }
 
 }
