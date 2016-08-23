cc21052e4b8b3784c932b4d3d91ffcd0167ad95f
Andreas Hennings <andreas@dqxtech.net>
2016-08-23 23:59:02 +0200

    Add DocUtil with ::docGetReturnTypeClassNames()

---
+++ cfrplugindiscovery/src/Util/DocUtil.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\cfrplugindiscovery\Util;
+
+final class DocUtil extends UtilBase {
+
+  /**
+   * @param string $docComment
+   * @param string $selfClassName
+   *
+   * @return string[]
+   */
+  public static function docGetReturnTypeClassNames($docComment, $selfClassName) {
+
+    if (!preg_match('~(?:^/\*\*\ +|\v\h*\* )@return\h+(\S+)~', $docComment, $m)) {
+      return [];
+    }
+
+    $names = [];
+    foreach (explode('|', $m[1]) as $alias) {
+      if ('' === $alias) {
+        continue;
+      }
+      if ('\\' === $alias[0] && preg_match('~(\\\\[a-zA-Z_][a-zA-Z_0-9]*)+~', $alias)) {
+        $names[] = substr($alias, 1);
+      }
+      elseif ('self' === $alias || 'static' === $alias) {
+        $names[] = $selfClassName;
+      }
+    }
+
+    return $names;
+  }
+
+}
