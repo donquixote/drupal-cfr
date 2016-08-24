3637b544c1c6d40ee8d86bab3f700f6773bbddeb
Andreas Hennings <andreas@dqxtech.net>
2016-08-24 06:58:09 +0200

    Remove unused ConfiguratorFilterInterface. #21

--- cfrapi/src/ConfiguratorFilter/ConfiguratorFilterInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Drupal\cfrapi\ConfiguratorFilter;
-
-use Drupal\cfrapi\Configurator\ConfiguratorInterface;
-
-interface ConfiguratorFilterInterface extends ConfiguratorInterface {
-
-}
