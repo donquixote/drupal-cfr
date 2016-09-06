36b88c8f7d983eb2915c679338e625c4aa3c657f
Andreas Hennings <andreas@dqxtech.net>
2016-09-06 05:17:17 +0200

    Add Configurator_CfrLegend.

---
+++ cfrfamily/src/Configurator/Composite/Configurator_CfrLegend.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Drupal\cfrfamily\Configurator\Composite;
+
+use Drupal\cfrapi\SummaryBuilder\SummaryBuilderInterface;
+use Drupal\cfrfamily\CfrLegend\CfrLegendInterface;
+use Drupal\cfrfamily\Configurator\Inlineable\InlineableConfiguratorInterface;
+use Drupal\cfrfamily\IdConfToValue\IdConfToValueInterface;
+
+class Configurator_CfrLegend extends Configurator_IdConfGrandBase implements InlineableConfiguratorInterface {
+
+  /**
+   * @var \Drupal\cfrfamily\CfrLegend\CfrLegendInterface
+   */
+  private $legend;
+
+  /**
+   * @var \Drupal\cfrfamily\IdConfToValue\IdConfToValueInterface
+   */
+  private $idConfToValue;
+
+  /**
+   * @param bool $required
+   * @param \Drupal\cfrfamily\CfrLegend\CfrLegendInterface $legend
+   * @param \Drupal\cfrfamily\IdConfToValue\IdConfToValueInterface $idConfToValue
+   */
+  public function __construct(
+    $required,
+    CfrLegendInterface $legend,
+    IdConfToValueInterface $idConfToValue
+  ) {
+    parent::__construct($required);
+    $this->legend = $legend;
+    $this->idConfToValue = $idConfToValue;
+  }
+
+  /**
+   * @return string[]|string[][]|mixed[]
+   */
+  protected function getSelectOptions() {
+
+    $options = [];
+    foreach ($this->legend->getLegendItems() as $id => $item) {
+      if (NULL === $groupLabel = $item->getGroupLabel()) {
+        $options[$id] = $item->getLabel();
+      }
+      else {
+        $options[$groupLabel][$id] = $item->getLabel();
+      }
+    }
+
+    return $options;
+  }
+
+  /**
+   * @param string $id
+   *
+   * @return string
+   */
+  protected function idGetLabel($id) {
+
+    if (NULL === $item = $this->legend->idGetLegendItem($id)) {
+      return $id;
+    }
+
+    return $item->getLabel();
+  }
+
+  /**
+   * @param string $id
+   * @param mixed $optionsConf
+   *
+   * @return array|null
+   */
+  protected function idConfGetOptionsForm($id, $optionsConf) {
+
+    if (NULL === $item = $this->legend->idGetLegendItem($id)) {
+      return NULL;
+    }
+
+    return $item->confGetForm($optionsConf, NULL);
+  }
+
+  /**
+   * @param string $id
+   * @param mixed $optionsConf
+   * @param \Drupal\cfrapi\SummaryBuilder\SummaryBuilderInterface $summaryBuilder
+   *
+   * @return string|null
+   */
+  protected function idConfGetSummary($id, $optionsConf, SummaryBuilderInterface $summaryBuilder) {
+
+    if (NULL === $legendItem = $this->legend->idGetLegendItem($id)) {
+      return '- ' . t('Unknown') . ' -';
+    }
+
+    $idLabel = $legendItem->getLabel();
+
+    return $summaryBuilder->idConf($idLabel, $legendItem, $optionsConf);
+  }
+
+  /**
+   * @param string $id
+   * @param mixed $optionsConf
+   *
+   * @return mixed
+   */
+  public function idConfGetValue($id, $optionsConf) {
+    return $this->idConfToValue->idConfGetValue($id, $optionsConf);
+  }
+
+  /**
+   * @return \Drupal\cfrfamily\CfrLegend\CfrLegendInterface|null
+   */
+  public function getCfrLegend() {
+    return $this->legend;
+  }
+}
