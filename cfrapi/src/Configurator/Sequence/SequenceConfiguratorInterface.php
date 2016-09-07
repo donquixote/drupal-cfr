3c0013cc77546dec339aea645d5ea271f258f4b4
Andreas Hennings <andreas@dqxtech.net>
2016-09-07 02:52:43 +0200

    Delete unused SequenceConfiguratorInterface.

--- cfrapi/src/Configurator/Sequence/SequenceConfiguratorInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Drupal\cfrapi\Configurator\Sequence;
-
-use Drupal\cfrapi\Configurator\ConfiguratorInterface;
-
-interface SequenceConfiguratorInterface extends ConfiguratorInterface {
-
-}
