d38c3dc3102a379c0ed207e530219bf404c1fd58
Andreas Hennings <andreas@dqxtech.net>
2016-10-11 03:37:10 +0200

    Add CfrInvalidArgumentException.

---
+++ cfrreflection/src/Exception/CfrInvalidArgumentException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Drupal\cfrreflection\Exception;
+
+use Drupal\cfrapi\Exception\InvalidConfigurationException;
+
+class CfrInvalidArgumentException extends InvalidConfigurationException {
+
+}
