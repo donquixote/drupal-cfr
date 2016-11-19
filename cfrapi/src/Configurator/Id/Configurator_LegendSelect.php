93a5ba8ad8461f49057336f2efcee897df3899f2
Andreas Hennings <andreas@dqxtech.net>
2016-11-19 02:06:31 +0100

    Create Configurator_LegendSelectBase factored out of Configurator_LegendSelect.

--- cfrapi/src/Configurator/Id/Configurator_LegendSelect.php
+++ cfrapi/src/Configurator/Id/Configurator_LegendSelect.php
@@ -4,12 +4,7 @@ namespace Drupal\cfrapi\Configurator\Id;
 
 use Drupal\cfrapi\Legend\LegendInterface;
 
-class Configurator_LegendSelect extends Configurator_SelectBase {
-
-  /**
-   * @var \Drupal\cfrapi\EnumMap\EnumMapInterface
-   */
-  private $legend;
+class Configurator_LegendSelect extends Configurator_LegendSelectBase {
 
   /**
    * @param \Drupal\cfrapi\Legend\LegendInterface $legend
@@ -29,40 +24,5 @@ class Configurator_LegendSelect extends Configurator_SelectBase {
    */
   public static function createOptional(LegendInterface $legend, $defaultId = NULL) {
     return new self($legend, FALSE, $defaultId);
-  }
-
-  /**
-   * @param \Drupal\cfrapi\Legend\LegendInterface $legend
-   * @param bool $required
-   * @param string|null $defaultId
-   */
-  public function __construct(LegendInterface $legend, $required = TRUE, $defaultId = NULL) {
-    $this->legend = $legend;
-    parent::__construct($required, $defaultId);
-  }
-
-  /**
-   * @return string[]|string[][]|mixed[]
-   */
-  protected function getSelectOptions() {
-    return $this->legend->getSelectOptions();
-  }
-
-  /**
-   * @param string $id
-   *
-   * @return string
-   */
-  protected function idGetLabel($id) {
-    return $this->legend->idGetLabel($id);
-  }
-
-  /**
-   * @param string $id
-   *
-   * @return bool
-   */
-  protected function idIsKnown($id) {
-    return $this->legend->idIsKnown($id);
   }
 }
