8a8a76d7c6721f5da0269957311857a9af0d298a
Andreas Hennings <andreas@dqxtech.net>
2016-12-24 02:18:30 +0100

    Introduce IdToConfigurator_ViaDefinition as a copy of IdToConfigurator_FromDefinitionMap, with more fine-grained dependency.
    It depends on the weaker IdToDefinitionInterface, instead of the stricter DefinitionMapInterface.
    
    (The latter extends the former and adds more methods.)

---
+++ cfrfamily/src/IdToConfigurator/IdToConfigurator_ViaDefinition.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Drupal\cfrfamily\IdToConfigurator;
+
+use Drupal\cfrapi\Configurator\Broken\BrokenConfiguratorInterface;
+use Drupal\cfrapi\Context\CfrContextInterface;
+use Drupal\cfrfamily\DefinitionToConfigurator\DefinitionToConfiguratorInterface;
+use Drupal\cfrfamily\IdToDefinition\IdToDefinitionInterface;
+
+class IdToConfigurator_ViaDefinition implements IdToConfiguratorInterface {
+
+  /**
+   * @var \Drupal\cfrfamily\IdToDefinition\IdToDefinitionInterface
+   */
+  private $idToDefinition;
+
+  /**
+   * @var \Drupal\cfrfamily\DefinitionToConfigurator\DefinitionToConfiguratorInterface
+   */
+  private $definitionToConfigurator;
+
+  /**
+   * @var \Drupal\cfrapi\Context\CfrContextInterface|null
+   */
+  private $context;
+
+  /**
+   * @param \Drupal\cfrfamily\IdToDefinition\IdToDefinitionInterface $definitionMap
+   * @param \Drupal\cfrfamily\DefinitionToConfigurator\DefinitionToConfiguratorInterface $definitionToConfigurator
+   * @param \Drupal\cfrapi\Context\CfrContextInterface|null $context
+   */
+  public function __construct(
+    IdToDefinitionInterface $definitionMap,
+    DefinitionToConfiguratorInterface $definitionToConfigurator,
+    CfrContextInterface $context = NULL
+  ) {
+    $this->idToDefinition = $definitionMap;
+    $this->definitionToConfigurator = $definitionToConfigurator;
+    $this->context = $context;
+  }
+
+  /**
+   * @param string|int $id
+   *
+   * @return null|\Drupal\cfrapi\Configurator\ConfiguratorInterface
+   */
+  public function idGetConfigurator($id) {
+
+    $definition = $this->idToDefinition->idGetDefinition($id);
+
+    if (NULL === $definition) {
+      return NULL;
+    }
+
+    $configurator = $this->definitionToConfigurator->definitionGetConfigurator($definition, $this->context);
+
+    if (NULL === $configurator) {
+      return NULL;
+    }
+
+    if ($configurator instanceof BrokenConfiguratorInterface) {
+      return NULL;
+    }
+
+    return $configurator;
+  }
+}
