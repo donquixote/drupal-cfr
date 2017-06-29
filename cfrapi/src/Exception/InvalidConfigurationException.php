e05014fa31573cc9228427e1d729a9cd161e8daf
Andreas Hennings <andreas@dqxtech.net>
2017-06-29 21:40:29 +0200

    (code style) Add a class doc on InvalidConfigurationException.

--- cfrapi/src/Exception/InvalidConfigurationException.php
+++ cfrapi/src/Exception/InvalidConfigurationException.php
@@ -2,6 +2,10 @@
 
 namespace Drupal\cfrapi\Exception;
 
+/**
+ * An exception that expresses that a configuration provided is incompatible
+ * with the current expectations of a configuration-to-value component.
+ */
 class InvalidConfigurationException extends ConfToValueException {
 
 }
