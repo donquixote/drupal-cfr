160456975dfc1ff1c3db68ebb027e87ad49c3220
Andreas Hennings <andreas@dqxtech.net>
2017-07-21 08:42:34 +0200

    Introduce StaUtil.

---
+++ cfrapi/cf/Util/StaUtil.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Donquixote\Cf\Util;
+
+use Donquixote\Cf\Emptyness\EmptynessInterface;
+use Donquixote\Cf\Evaluator\P2\EvaluatorP2Interface;
+use Donquixote\Cf\Schema\CfSchemaInterface;
+use Donquixote\Cf\SchemaToAnything\SchemaToAnythingInterface;
+
+final class StaUtil extends UtilBase {
+
+  /**
+   * @param \Donquixote\Cf\Schema\CfSchemaInterface $schema
+   * @param \Donquixote\Cf\SchemaToAnything\SchemaToAnythingInterface $schemaToAnything
+   *
+   * @return \Donquixote\Cf\Evaluator\P2\EvaluatorP2Interface
+   */
+  public static function evaluatorP2(CfSchemaInterface $schema, SchemaToAnythingInterface $schemaToAnything) {
+    return self::getObject($schema, $schemaToAnything, EvaluatorP2Interface::class);
+  }
+
+  /**
+   * @param \Donquixote\Cf\Schema\CfSchemaInterface $schema
+   * @param \Donquixote\Cf\SchemaToAnything\SchemaToAnythingInterface $schemaToAnything
+   *
+   * @return \Donquixote\Cf\Emptyness\EmptynessInterface
+   */
+  public static function emptyness(CfSchemaInterface $schema, SchemaToAnythingInterface $schemaToAnything) {
+    return self::getObject($schema, $schemaToAnything, EmptynessInterface::class);
+  }
+
+  /**
+   * @param \Donquixote\Cf\Schema\CfSchemaInterface $schema
+   * @param \Donquixote\Cf\SchemaToAnything\SchemaToAnythingInterface $schemaToAnything
+   * @param string $interface
+   *
+   * @return mixed|null
+   */
+  private static function getObject(CfSchemaInterface $schema, SchemaToAnythingInterface $schemaToAnything, $interface) {
+
+    $object = $schemaToAnything->schema($schema, $interface);
+
+    if (NULL === $object || !$object instanceof $interface) {
+      return NULL;
+    }
+
+    return $object;
+  }
+
+}
