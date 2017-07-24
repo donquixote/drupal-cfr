ba5ddad4aa0c95df58bf0e4e0641ff09828d2291
Andreas Hennings <andreas@dqxtech.net>
2017-07-24 02:15:53 +0200

    Delete all code from old partials architecture.

--- cfrapi/cf/Evaluator/Helper/EvaluatorHelperBaseInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Donquixote\Cf\Evaluator\Helper;
-
-use Donquixote\Cf\Helper\SchemaHelperBaseInterface;
-
-interface EvaluatorHelperBaseInterface extends SchemaHelperBaseInterface {
-
-}
