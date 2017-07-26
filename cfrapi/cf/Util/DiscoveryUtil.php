0058430c07de07cac4421d3c08810739d3dda2e4
Andreas Hennings <andreas@dqxtech.net>
2017-07-26 04:07:55 +0200

    Remove debug / testing methods.

--- cfrapi/cf/Util/DiscoveryUtil.php
+++ cfrapi/cf/Util/DiscoveryUtil.php
@@ -4,53 +4,10 @@ namespace Donquixote\Cf\Util;
 
 use Donquixote\Cf\Discovery\AnnotatedFactory;
 use Donquixote\Cf\Discovery\ClassFilesIAInterface;
-use Donquixote\Cf\Emptiness\EmptinessInterface;
-use Donquixote\Cf\Evaluator\EvaluatorInterface;
 use Donquixote\Cf\ParamToValue\ParamToValue_ObjectsMatchType;
-use Donquixote\Cf\Schema\Optional\CfSchema_OptionalInterface;
-use Donquixote\Cf\Schema\Options\CfSchema_Options_Fixed;
 use Donquixote\Cf\Translator\Translator;
-use Drupal\cfrplugin\Hub\CfrPluginHub;
-use Drupal\cfrplugin\Util\UiDumpUtil;
 
 final class DiscoveryUtil extends UtilBase {
-
-  public static function test_2() {
-    $container = CfrPluginHub::getContainer();
-    $sta = $container->schemaToAnything;
-    $schema = CfSchema_Options_Fixed::createFlat(
-      [
-        'a' => 'A',
-        'b' => 'B',
-      ]);
-    $emptyness = $sta->schema($schema, EmptinessInterface::class);
-    dpm($emptyness);
-  }
-
-  public static function test_0() {
-    $services = [];
-    $services[] = Translator::createPassthru();
-    $paramToValue = new ParamToValue_ObjectsMatchType($services);
-    $partials = LocalPackageUtil::collectSTAPartials($paramToValue);
-    $filtered = STAMappersUtil::filterPartialsBySchemaType($partials, CfSchema_OptionalInterface::class);
-    $ff = STAMappersUtil::filterPartialsByTargetType($filtered, EvaluatorInterface::class);
-    kdpm($filtered);
-    kdpm($ff);
-  }
-
-  public static function test_1() {
-    $e = new \Exception();
-    UiDumpUtil::displayException($e);
-  }
-
-  public static function test() {
-
-    $paramToValue = self::getParamToValue();
-
-    $results = LocalPackageUtil::collectSTAPartials($paramToValue);
-
-    kdpm($results);
-  }
 
   /**
    * @return \Donquixote\Cf\ParamToValue\ParamToValueInterface
