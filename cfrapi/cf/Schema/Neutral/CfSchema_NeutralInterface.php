526efe9cb4acf4ddcfb74e63b693abbb942e0af6
Andreas Hennings <andreas@dqxtech.net>
2017-08-03 02:12:13 +0200

    Let CfSchema_NeutralInterface no longer extend CfSchema_TransformableInterface.

--- cfrapi/cf/Schema/Neutral/CfSchema_NeutralInterface.php
+++ cfrapi/cf/Schema/Neutral/CfSchema_NeutralInterface.php
@@ -3,10 +3,9 @@
 namespace Donquixote\Cf\Schema\Neutral;
 
 use Donquixote\Cf\Schema\SkipEvaluator\CfSchema_SkipEvaluatorInterface;
-use Donquixote\Cf\SchemaBase\CfSchema_TransformableInterface;
 use Donquixote\Cf\SchemaBase\CfSchema_ValueToValueBaseInterface;
 
-interface CfSchema_NeutralInterface extends CfSchema_TransformableInterface, CfSchema_ValueToValueBaseInterface, CfSchema_SkipEvaluatorInterface {
+interface CfSchema_NeutralInterface extends CfSchema_ValueToValueBaseInterface, CfSchema_SkipEvaluatorInterface {
 
   /**
    * @return \Donquixote\Cf\Schema\CfSchemaInterface
