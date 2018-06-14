e8c87401854ec0c241cf5e4ed76d7603aca68626
Andreas Hennings <andreas@dqxtech.net>
2018-06-14 23:57:06 +0200

    Exception handling in CfrReflectionUtil - always throw ConfToValueException.

--- cfrreflection/src/Util/CfrReflectionUtil.php
+++ cfrreflection/src/Util/CfrReflectionUtil.php
@@ -3,9 +3,15 @@
 namespace Drupal\cfrreflection\Util;
 
 use Donquixote\CallbackReflection\Callback\CallbackReflectionInterface;
-use Drupal\cfrapi\Exception\InvalidConfigurationException;
+use Drupal\cfrapi\Exception\ConfToValueException;
 use Drupal\cfrapi\Util\UtilBase;
 
+/**
+ * Utility class to deal with conf-to-value callbacks.
+ *
+ * All methods throw ConfToValueException on failure, assuming that they are
+ * only called in a conf-to-value context.
+ */
 final class CfrReflectionUtil extends UtilBase {
 
   /**
@@ -14,12 +20,12 @@ final class CfrReflectionUtil extends UtilBase {
    *
    * @return mixed
    *
-   * @throws \Drupal\cfrapi\Exception\InvalidConfigurationException
+   * @throws \Drupal\cfrapi\Exception\ConfToValueException
    */
   public static function callbackValidateAndInvoke(CallbackReflectionInterface $callback, $args) {
 
     if (!is_array($args)) {
-      throw new InvalidConfigurationException("Non-array callback arguments");
+      throw new ConfToValueException("Non-array callback arguments");
     }
 
     self::callbackAssertValidArgs($callback, $args);
@@ -27,11 +33,8 @@ final class CfrReflectionUtil extends UtilBase {
     try {
       return $callback->invokeArgs($args);
     }
-    catch (InvalidConfigurationException $e) {
-      throw $e;
-    }
     catch (\Exception $e) {
-      throw new InvalidConfigurationException('Exception during callback', NULL, $e);
+      throw new ConfToValueException('Exception during callback', NULL, $e);
     }
   }
 
@@ -39,14 +42,14 @@ final class CfrReflectionUtil extends UtilBase {
    * @param \Donquixote\CallbackReflection\Callback\CallbackReflectionInterface $callback
    * @param array $args
    *
-   * @throws \Drupal\cfrapi\Exception\InvalidConfigurationException
+   * @throws \Drupal\cfrapi\Exception\ConfToValueException
    */
   public static function callbackAssertValidArgs(CallbackReflectionInterface $callback, array $args) {
 
     $params = $callback->getReflectionParameters();
 
     if (array_keys($params) !== array_keys($args)) {
-      throw new InvalidConfigurationException('Wrong argument count.');
+      throw new ConfToValueException('Wrong argument count.');
     }
 
     foreach ($callback->getReflectionParameters() as $i => $param) {
@@ -56,9 +59,8 @@ final class CfrReflectionUtil extends UtilBase {
           // All following parameters are optional.
           return;
         }
-        else {
-          throw new InvalidConfigurationException("Required argument $i missing.");
-        }
+
+        throw new ConfToValueException("Required argument $i missing.");
       }
 
       $arg = $args[$i];
@@ -71,17 +73,17 @@ final class CfrReflectionUtil extends UtilBase {
 
       if ($param->isArray()) {
         if (!is_array($arg)) {
-          throw new InvalidConfigurationException("Argument $i must be an array.");
+          throw new ConfToValueException("Argument $i must be an array.");
         }
       }
 
       if ($paramClass = $param->getClass()) {
         if (!is_object($arg)) {
-          throw new InvalidConfigurationException("Argument $i must be an object.");
+          throw new ConfToValueException("Argument $i must be an object.");
         }
         if (!$paramClass->isInstance($arg)) {
           $paramClassExport = $paramClass->getName();
-          throw new InvalidConfigurationException("Argument $i must be an instance of $paramClassExport.");
+          throw new ConfToValueException("Argument $i must be an instance of $paramClassExport.");
         }
       }
     }
